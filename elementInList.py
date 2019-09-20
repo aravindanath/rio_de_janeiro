ede689c5610de04ebb401636b5195499f7b4a0b5
Deepak8763 <deepak.dkky@gmail.com>
2019-09-20 09:55:41 +0530

    Finding the element in the list Deepak

---
+++ elementInList.py
@@ -0,0 +1,10 @@
+list = [3,5,16,8,25,77]
+print("Checking  5 exists in list or not")
+for i in list:
+    if(i == 5):
+        print("Element is Exists in the List")
+    elif(i == 3):
+        print("Element is Exists in the List")
+    else:
+        print("Element is not existing")
+
