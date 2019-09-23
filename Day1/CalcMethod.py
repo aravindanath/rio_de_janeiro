a406285b8594585275f665331e39add83cb8408c
bhuvi196 <bhuvi196@gmail.com>
2019-09-23 17:38:05 +0530

    CalcMethod

---
+++ Day1/CalcMethod.py
@@ -0,0 +1,20 @@
+#bhuvaneswari
+# declaring methods to calculate
+def addition(a,b):
+    x=a+b
+    print("Addition of given numbers",x)
+def subtraction(a,b):
+    x=a-b
+    print("Subtraction of given numbers",x)
+def Multiplication(a,b):
+    x=a*b
+    print("multiplication of given numbers",x)
+def Division(a,b):
+    x=a/b
+    print("Division of given numbers",x)
+
+ # passing arguments to do the calculation
+addition(30,50)
+subtraction(90,45)
+Multiplication(12,15)
+Division(10,2)
